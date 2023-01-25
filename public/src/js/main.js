e87c9bf9051ae78f877f1286eebaac7c72c0821d
deivis17 <marchandeivis@gmail.com>
2023-01-25 14:23:37 -0400

    Header finished 2:23P.M.

---
+++ public/src/js/main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.services', {delay:500});
+ScrollReveal().reveal('.about-us', {delay:500});
+ScrollReveal().reveal('.start-ups', {delay:500});
+ScrollReveal().reveal('.footer', {delay:500});
