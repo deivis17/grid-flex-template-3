6a5d07cace15e41ee9876f392362f32857ff3ca5
deivis17 <marchandeivis@gmail.com>
2023-01-26 19:44:41 -0400

    Cookies finished

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,3 +1,30 @@
+const clickme = document.querySelector('#clickme');
+const cookies = document.querySelector('#cookies');
+const showForm = document.querySelector('#showform');
+const formsignin = document.querySelector('#formsignin');
+
+
+clickme.addEventListener('click', changeColor);
+showForm.addEventListener('click', console.log('funcionando'));
+
+function signIn(){
+    form.classList.toggle('show');
+}
+
+function changeColor(){
+    cookies.classList.toggle('hide');
+}
+
+
+
+
+
+
+
+
+
+
+
 ScrollReveal().reveal('.services', {delay:500});
 ScrollReveal().reveal('.about-us', {delay:500});
 ScrollReveal().reveal('.start-ups', {delay:500});
