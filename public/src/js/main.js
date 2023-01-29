97d1342c4a43a12e0ccaeacb9877b2d90c8959c2
deivis17 <marchandeivis@gmail.com>
2023-01-29 00:52:24 -0400

    Main.js resolved

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,21 +1,23 @@
-const clickme = document.querySelector('#clickme');
-const cookies = document.querySelector('#cookies');
-const showForm = document.querySelector('#showform');
-const formsignin = document.querySelector('#formsignin');
-
-
-clickme.addEventListener('click', hideCookies);
-showForm.addEventListener('click', signIn);
-
-function signIn(){
-    form.classList.toggle('show');
-}
-
-function hideCookies(){
-    cookies.classList.toggle('hide');
-}
-
-ScrollReveal().reveal('.services', {delay:500});
-ScrollReveal().reveal('.about-us', {delay:500});
-ScrollReveal().reveal('.start-ups', {delay:500});
-ScrollReveal().reveal('.footer', {delay:500});
+window.onload = () => {
+    const clickme = document.querySelector('#clickme');
+    const cookies = document.querySelector('#cookies');
+    const showForm = document.querySelector('#showform');
+    const formsignin = document.querySelector('#formsignin');
+    
+    
+    clickme.addEventListener('click', hideCookies);
+    showForm.addEventListener('click', signIn);
+    
+    function signIn(){
+        formsignin.classList.toggle('show');
+    }
+    
+    function hideCookies(){
+        cookies.classList.toggle('hide');
+    }
+    
+    ScrollReveal().reveal('.services', {delay:500});
+    ScrollReveal().reveal('.about-us', {delay:500});
+    ScrollReveal().reveal('.start-ups', {delay:500});
+    ScrollReveal().reveal('.footer', {delay:500});
+    }
